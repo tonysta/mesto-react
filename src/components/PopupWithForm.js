671da83fca3f8aa5732f4cdfe20a8291f04df7a9
anton startsev <anton.startsev@osome.com>
2022-06-07 23:59:48 +0400

    fix css

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function PopupWithForm({props, children}) {
+function PopupWithForm({name, title, children}) {
     return(
-        <div className={`popup popup_section_${props.name}`}>
+        <div className={`popup popup_section_${name}`}>
             <div className="popup__container">
                 <button type="button" className="popup__close-btn"></button>
-                <form name={props.name} className="popup__form popup__form_type_profile" noValidate>
-                    <h2 className="popup__title">Редактировать профиль</h2>
+                <form name={name} className="popup__form popup__form_type_profile" noValidate>
+                    <h2 className="popup__title">{title}</h2>
                     {children}
                     <button type="submit" className="popup__submit">Сохранить</button>
                 </form>
