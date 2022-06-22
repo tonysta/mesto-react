c507e6dc88d8e3f0b86b79d50f9bfff3309ffb17
anton startsev <anton.startsev@osome.com>
2022-06-23 01:56:12 +0300

    card button trash, like

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,19 @@
 import React from "react";
+import {CurrentUserContext} from "../contexts/CurrentUserContext";
 
 function Card({card, onCardClick}) {
+    const currentUser = React.useContext(CurrentUserContext);
+    const isOwn = card.owner._id === currentUser._id;
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const cardLikeButtonClassName = `card__like-btn ${isLiked ? 'card__like-btn_active' : ''}`;
     return(
         <div className="card" >
-            <button type="button" className="card__trash-btn"></button>
+            {isOwn ? <button type="button" className="card__trash-btn"></button>:null}
             <img src={card.link} alt={card.name} className="card__img" onClick={() => onCardClick(card)}/>
             <div className="card__heading">
                 <h2 className="card__title">{card.name}</h2>
                 <div className="card__like-container">
-                    <button type="button" className="card__like-btn"></button>
+                    <button type="button" className={cardLikeButtonClassName}></button>
                     <span className="card__like-counter">{card.likes.length}</span>
                 </div>
             </div>
